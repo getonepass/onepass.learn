bce3116b018c2f43099b3c0997df2245f406be47
Jorge Fernández <jorge.fernandez@cores.gl>
2025-03-11 22:13:50 +0100

    Initial commit

---
+++ decentralized-identity/core-concepts-&-standards/_meta.tsx
@@ -0,0 +1,11 @@
+import type { MetaRecord } from "nextra"
+
+export default {
+  "self-sovereign-identity-explained":
+    "Self-Sovereign Identity (SSI) Explained",
+  "w3c-standards-for-decentralized-identity":
+    "W3C Standards for Decentralized Identity",
+  "understanding-decentralized-identifiers":
+    "Understanding Decentralized Identifiers (DIDs)",
+  "what-are-verifiable-credentials": "What Are Verifiable Credentials (VCs)?",
+} satisfies MetaRecord
